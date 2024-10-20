b3a1bbf425c330f6042b1d4baa9d1e4c33265ff0
PythonMania2024 <igjaguzkra@gmail.com>
2024-10-20 18:56:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import pygame
 
+pygame.init()
+pygame.display.set_mode(())
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT():
+      import sys
+      sys.exit()
